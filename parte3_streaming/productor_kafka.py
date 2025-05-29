7200d7f224684f61bd2c521bc19f24c4ed7eb2c8
EliJohana7 <eli.johana7@gmail.com>
2025-05-29 19:28:01 +0000

    producer

---
+++ parte3_streaming/productor_kafka.py
@@ -0,0 +1,17 @@
+from kafka import KafkaProducer
+import json
+import time
+-+
+producer = KafkaProducer(
+    bootstrap_servers='195.235.211.197:9092',
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+with open("./parte3_streaming/streaming_sample.json", "r") as f:
+    for line in f:
+        mensaje = json.loads(line)
+        producer.send("energyConsumption", value=mensaje)
+        print("Enviado:", mensaje)
+        time.sleep(1)  
+
+producer.flush()
