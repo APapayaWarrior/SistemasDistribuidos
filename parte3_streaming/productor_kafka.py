8466260300d9711688dc4aaadde50e1c5450fdc8
APapayaWarrior <dadhoz@gmail.com>
2025-05-30 23:39:40 +0200

    resolviendo merge

--- parte3_streaming/productor_kafka.py
+++ parte3_streaming/productor_kafka.py
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-from kafka import KafkaProducer
-import json
-import time
--+
-producer = KafkaProducer(
-    bootstrap_servers='195.235.211.197:9092',
-    value_serializer=lambda v: json.dumps(v).encode('utf-8')
-)
-
-with open("./parte3_streaming/streaming_sample.json", "r") as f:
-    for line in f:
-        mensaje = json.loads(line)
-        producer.send("energyConsumption", value=mensaje)
-        print("Enviado:", mensaje)
-        time.sleep(1)  
-
-producer.flush()
-=======
 from kafka import KafkaProducer
 import json
 import time
@@ -35,4 +16,3 @@ with open("./parte3_streaming/streaming_sample.json", "r") as f:
         time.sleep(1)  
 
 producer.flush()
->>>>>>> fe4e2e05caf1f00f93f59ec945ce106265c541e0
